2e5009633d9026b188114cbc57431062921b7044
takxxx-infra <takxxxaaa+aws1@gmail.com>
2026-02-11 00:03:57 +0900

    update: create ecs deployment role

--- terraform/iam_role.tf
+++ terraform/iam_role.tf
@@ -29,3 +29,29 @@ resource "aws_iam_instance_profile" "ec2_management" {
   name = "PseudoCloud9IamRole"
   role = aws_iam_role.ec2_management.name
 }
+
+# ##################################################
+# ECS Blue/Green Deployment
+# ##################################################
+resource "aws_iam_role" "ecs_deployment" {
+  name = "EcsInfrastructureRoleForLoadBalancers"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_deployment" {
+  for_each = {
+    ecs         = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
+    ecs_for_alb = "arn:aws:iam::aws:policy/AmazonECSInfrastructureRolePolicyForLoadBalancers"
+  }
+  role       = aws_iam_role.ecs_deployment.name
+  policy_arn = each.value
+}
